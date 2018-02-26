1d0ccca331fbf2f97641eac69d42fb989384a8da
Mark Hao <mark@Marks-MacBook-Pro.local>
2018-02-26 11:29:45 -0800

    Create Book & Bookshelf components

---
+++ src/components/objects/Book.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Book extends React.Component {
+  render () {
+    return(
+      <div>This is a Book</div>
+    )
+  }
+}
+
+export default Book;
